52d883ad15f7d53fab05ca7dd6423178808c10a4
justintsugranes <justintsugranes@proton.me>
2022-07-22 05:10:51 -0600

    account edit

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,7 +12,7 @@ module.exports = {
     },
     goerli: {
       url: "https://goerli.infura.io/v3/3fe57477a6a540c59de90851245b8d37",
-      accounts: ["0x${0xF40584ebbd6Acef9B362ee3367F5347E9Bd9bB61}"],
+      accounts: ["0x${}"],
     },
   },
 };
